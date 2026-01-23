54c9fcf9ae7a689156e7cbd408de1afdd1e54927
l-krairit <124411749+l-krairit@users.noreply.github.com>
2026-01-23 11:58:43 +0700

    Fixed mistakes for lab 1

--- lab-1/main.py
+++ lab-1/main.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 
 from typing import List
 
-from Crypto.Hash import SHA1
+from Crypto.Hash import SHA256
 
 BLOCK_SIZE = 16
 
@@ -17,22 +17,20 @@ def chunk_bytes(data: bytes, block_size: int = BLOCK_SIZE) -> List[bytes]:
 
 # TODO: Modify this function
 def compute_integrity_fingerprint(data: bytes) -> str:
-    blocks = chunk_bytes(data, BLOCK_SIZE)
+    h = SHA256.new()
 
-    h = SHA1.new()
-    for block in blocks:
-        h = SHA1.new(str(block).encode("utf-8"))
-        h.digest()
+    for block in chunk_bytes(data):
+        h.update(block)
 
-    return h.digest()
+    return h.hexdigest()
 
 def verify_integrity(data: bytes, expected_hex: str) -> bool:
     actual = compute_integrity_fingerprint(data)
-    return actual == expected_hex
+    return actual == expected_hex.strip()
 
 if __name__ == "__main__":
     # Assume we downloaded this fingerprint from the file author.
-    true_fp = "da39a3ee5e6b4b0d3255bfef95601890afd80709\n"
+    true_fp = "7682bf3e6fbde3d7a0926937cc5d2c90784f6db09c41629ade03a61498b4664a\n"
     print("True fingerprint:", true_fp)
 
     # Assume we read this file from disk.
@@ -49,5 +47,5 @@ if __name__ == "__main__":
     tampered_file_a_fp = compute_integrity_fingerprint(tampered_file_a)
     print("Computed fingerprint (tampered):", tampered_file_a_fp)
 
-    print("Fingerprint verification (tampered):", verify_integrity(tampered_file_a_fp, true_fp))
-
+    print("Fingerprint verification (tampered):", verify_integrity(tampered_file_a, true_fp))
+  
